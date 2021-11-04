26aaeb1ab6745d275a3d09aff508e319edf56dc7
Jason Qiu <jqiu21@protonmail.com>
2021-11-03 20:19:04 -0400

    Added recursive Fibonacci to lab 10

--- lab/lab10.py
+++ lab/lab10.py
@@ -69,12 +69,48 @@ Fibonacci = """
 13 halt
 """
 
+FibRec = """
+# Input: n
+# Output: nth Fibbonacci number; f(0) = 1
+# Assume: n>=0; n is an int
+00 setn r15 42      # Set stack pointer
+01 setn r2 2
+02 read r1
+03 calln r14 06     # Initial function call
+04 write r13
+05 halt
+
+06 sub r3 r1 r2
+07 jgtzn r3 11
+08 setn r13 1
+09 setn r12 1
+10 jumpr r14
+
+11 pushr r1 r15     # Recursive function call 1 (fib(n-1))
+12 pushr r14 r15
+13 addn r1 -1
+14 calln r14 06
+15 popr r14 r15
+16 popr r1 r15
+
+17 pushr r1 r15     # Recursive function call 2 (fib(n-2))
+18 pushr r14 r15
+19 addn r1 -1
+20 calln r14 06
+21 popr r14 r15
+22 popr r1 r15
+
+23 pushr r13 r15
+24 add r13 r13 r12
+25 popr r12 r15
+26 jumpr r14
+"""
 
 # ~~~~~ Running ~~~~~~
 import hmmm
 import importlib
 
-runThis = Max  # Change to the function you want to run
+runThis = FibRec  # Change to the function you want to run
 doDebug = True # Change to turn debug mode on/off
 
 # call main() from the command line to run the program again
